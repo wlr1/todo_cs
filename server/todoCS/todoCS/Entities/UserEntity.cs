80afa1e861feb45eef13ca059b329b7c651ea719
wlr1 <uns4d123@gmail.com>
2024-08-28 17:16:49 +0300

    ADDED: Dtos folder & Entities / UserDto & UserEntity files

---
+++ server/todoCS/todoCS/Entities/UserEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace todoCS.Entities;
+
+public class UserEntity
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? Username { get; set; }
+    
+    public string? PasswordHash { get; set; }
+    
+  
+}
