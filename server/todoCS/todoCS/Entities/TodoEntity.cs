546149e44b03c0b66744dde2683beca9ce6effc6
wlr1 <uns4d123@gmail.com>
2024-08-30 23:22:32 +0300

    ADDED: Entity and Dto for todo

---
+++ server/todoCS/todoCS/Entities/TodoEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace todoCS.Entities;
+
+public class TodoEntity
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+    
+}
