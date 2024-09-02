18deec24fd0b0efecf9d3e0f9cfd4d680a0a7b1d
wlr1 <uns4d123@gmail.com>
2024-09-02 17:49:42 +0300

    ADDED: Create/Update/Delete routes

--- server/todoCS/todoCS/Dtos/TodoDto.cs
+++ server/todoCS/todoCS/Dtos/TodoDto.cs
@@ -1,9 +1,13 @@
-﻿namespace todoCS.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace todoCS.Dtos;
 
 public class TodoDto
 {
     public long Id { get; set; }
+   
     public string Title { get; set; }
+ 
     public string Description { get; set; }
     public bool IsCompleted { get; set; } = false;
     public DateTime CreatedAt { get; set; }
--- server/todoCS/todoCS/Entities/TodoEntityItem.cs
+++ server/todoCS/todoCS/Entities/TodoEntityItem.cs
@@ -1,11 +1,14 @@
-﻿using System.Runtime.InteropServices.JavaScript;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
 
 namespace todoCS.Entities;
 
 public class TodoEntityItem
 {
     public long Id { get; set; }
+ 
     public string Title { get; set; }
+
     public string Description { get; set; }
     public bool IsCompleted { get; set; } = false;
     public DateTime CreatedAt { get; set; }
